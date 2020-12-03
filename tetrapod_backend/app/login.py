306d09a3a1ae7999bd50245931c1290870bde16f
gjp4tw <gjp40217@gmail.com>
2020-12-03 20:41:50 +0800

    feat: token validation

--- tetrapod_backend/app/login.py
+++ tetrapod_backend/app/login.py
@@ -4,16 +4,15 @@ from ..lib import config
 import time
 import jwt
 
-COLLECTION = "account"
-
 @app.route("/login",methods=["POST"])
 def _login():
     _account = request.get_json().get("account",None)
     _password = request.get_json().get("password",None)
-    MODEL = account.Account(COLLECTION)
-    req = MODEL._get({"account":_account})
+    MODEL = account.Account()
+    req = MODEL.get({"account":_account})
     secret = config.getConfig().get("app",{}).get("secret")
-    token = jwt.encode({"account":_account,"timestamp":str(time.time())},secret)
+    token = bytes.decode(jwt.encode({"account":_account,"timestamp":str(time.time())},secret))
+    MODEL.update({"account":_account},{"$set": {"token": token}})
     if req != None and _account == req['account'] and _password == req['password']:
-        return make_response(jsonify({"token":bytes.decode(token)}),200)
+        return make_response(jsonify({"token":token}),200)
     return make_response(jsonify("login failure"), 401)
