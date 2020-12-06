3439bb14154233850aa3086da2ceab4d521d69a9
Andy000002 <00757113@email.ntou.edu.tw>
2020-12-07 01:47:53 +0800

    delete annotation

--- tetrapod_backend/app/upload_merchant.py
+++ tetrapod_backend/app/upload_merchant.py
@@ -7,7 +7,6 @@ import json
 @app.route("/upload_merchant",methods=["POST"])
 @account.Account.validate
 def _upload_merchant():
-    # get form ,pop token
     form=request.get_json()
     secret = config.getConfig().get("app",{}).get("secret",None)
     _token = form.get("token",None)
@@ -16,31 +15,6 @@ def _upload_merchant():
     form.pop('token',None)
     form["account"]=_account
     json.dumps(form)
-    #insert merchant db
     MODEL = merchant.Merchant()
     MODEL.new(form)
     return make_response(jsonify("upload merchant seccess"),200)
-
-    # POST
-    # "price":"4129889",
-    # "photo":"",
-    # "merchant_name":"textbook",
-    # "count":"20",
-    # "discription":"It's a book",
-    # "is_bidding":"0",
-    # "bidding_price":"",
-    # "bidding_price_perbid":"",
-    # "bidding_endtime":"",
-    # "token": ""
-
-    # to DB
-    # "account":"aaaaa"
-    # "price":"4129889",
-    # "photo":"",
-    # "merchant_name":"textbook",
-    # "count":"20",
-    # "discription":"It's a book",
-    # "is_bidding":"0",
-    # "bidding_price":"",
-    # "bidding_price_perbid":"",
-    # "bidding_endtime":""
