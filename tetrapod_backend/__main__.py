e7ed12589e2d60f7795e5a5790fd2099f115644e
willy10155170 <willy10155170@gmail.com>
2020-12-07 04:11:27 +0800

    feat: delete merchant

--- tetrapod_backend/__main__.py
+++ tetrapod_backend/__main__.py
@@ -19,7 +19,7 @@ def run_app():
     # THIS IS BADDDDD SOOOO FUCKING BADDDDD, BUT I AM JUST 2 LAZY 2 MAKE IT LEGIT
     
     _LOGGER.info("[init] Registering APIs by a nasty way...")
-    from .app import testAPI, login, register, edit_account, logout
+    from .app import app, testAPI, login, register, edit_account, upload_merchant, delete_merchant
     app_host = CONF.get("app", {}).get("host", "127.0.0.1")
     app_port = CONF.get("app", {}).get("port", 9000)
     app.app.run(host=app_host, port=app_port)
