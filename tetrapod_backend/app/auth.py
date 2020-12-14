b116decd7b7d3632fe35e1a4ad8617ad952d1cdf
falcon 草 <falcon.lee1011@gmail.com>
2020-12-15 04:37:56 +0800

    fix: API sort, old API sources are moved to /app/legacy

---
+++ tetrapod_backend/app/auth.py
@@ -0,0 +1,312 @@
+from ..db.models import account, token, verification
+from .app import *
+from ..lib import config
+import jwt, re, random, string, logging
+from time import time
+from flask_mail import Message
+from ..lib.config import getConfig
+
+_LOGGER = logging.getLogger()
+
+MODULE_PREFIX = '/auth'
+account_MODEL = account.Account()
+verification_MODEL = verification.Verification()
+
+def _len_check(s):
+    if len(s) > 5 and len(s) < 21:
+        return True
+    else:
+        return False
+
+def _different_password(str1, str2):
+    if str1 == str2:
+        return True
+    else:
+        return False
+
+def _is_match(s, pat):
+    return re.findall(pat, s)
+
+@app.route(f"{MODULE_PREFIX}/editaccount",methods=["POST"])
+@account.Account.validate
+def _edit_account(*args,**kwargs):    
+    data = request.get_json()
+    _account_avator = data.get("account_avator","")
+    _First_name = data.get("first name","")
+    _Last_name = data.get("last name","")
+    _Nick_name = data.get("nick name","")
+    _password = data.get("password","")
+    _confirm_password = data.get("confirm password","")
+    _email = data.get("e-mail","")
+    _phone = data.get("phone","")
+    _birth_date = data.get("birth date","")
+    _sex = data.get("sex","")
+    _description = data.get("market description","")
+    account_MODEL = account.Account()
+        
+    #password check 英數，至少6碼至多20碼
+    pattern = r"[a-zA-Z0-9]+$"
+    DP = _different_password(_password, _confirm_password)
+    LC = _len_check(_password)
+    match = _is_match(_password, pattern)
+    if not (DP and LC and match):
+        Err = "password format error"
+        return make_response(jsonify({"status": Err}), 200)
+    
+    #e-mail check
+    req = account_MODEL.get({"e-mail":_email})
+    if req != None:
+        Err = "e-mail already exists"
+        return make_response(jsonify({"status": Err}), 200)
+    pattern = r"^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$"
+    match = _is_match(_email, pattern)
+    if not match:
+        Err = "E-mail format error"
+        return make_response(jsonify({"status": Err}), 200)
+
+    #phone check
+    pattern = r"^09\d{8}$"
+    match = _is_match(_phone, pattern)
+    if not match:
+        Err = "phone format error"
+        return make_response(jsonify({"status": Err}), 200)
+
+    #make json
+    user = {
+        "account_avator": _account_avator,
+        "first_name": _First_name,
+        "last_name":  _Last_name,
+        "nick_name": _Nick_name,
+        "password": _password,
+        "e-mail": _email,
+        "phone": _phone,
+        "birth_date": _birth_date,
+        "sex": _sex,
+        "market_description": _description
+    }
+    account_req = account_MODEL.update({"account":kwargs["account"]},{"$set":user})
+    return make_response(jsonify("edit account success"),200)
+
+@app.route(f"{MODULE_PREFIX}/register",methods=["POST"])
+def _register():
+    def _len_check(s):
+        if len(s) > 5 and len(s) < 21:
+            return True
+        else:
+            return False
+
+    def _different_password(str1, str2):
+        if str1 == str2:
+            return True
+        else:
+            return False
+
+    def _is_match(s, pat):
+        return re.match(pat, s)
+
+    data = request.get_json()   
+    _First_name = data.get("first name","")
+    _Last_name = data.get("last name","")
+    _Nick_name = data.get("nick name","")
+    _account = data.get("account","")
+    _password = data.get("password","")
+    _confirm_password = data.get("confirm password","")
+    _email = data.get("e-mail","")
+    _phone = data.get("phone","")
+    Pass = True
+    Err = ""
+    MODEL = account.Account()
+    
+    #account check 英文大小寫開頭+英數，至少6碼至多20碼
+    req = MODEL.get({"account":_account})
+    if req != None:
+        Err = "account already exists"
+        return make_response(jsonify({"status": Err}), 200)
+    pattern = r"[a-zA-Z]+[a-zA-Z0-9]+$"
+    match = _is_match(_account, pattern)
+    LC = _len_check(_account)
+    if match == None or not LC:
+        Err = "account format error"
+        return make_response(jsonify({"status": Err}), 200)
+        
+    #password check 英數，至少6碼至多20碼
+    pattern = r"[a-zA-Z0-9]+$"
+    DP = _different_password(_password, _confirm_password)
+    LC = _len_check(_password)
+    match = _is_match(_password, pattern)
+    if not DP or not LC or match == None:
+        Err = "password format error"
+        return make_response(jsonify({"status": Err}), 200)
+    
+    #e-mail check
+    req = MODEL.get({"e-mail":_email})
+    if req != None:
+        Err = "email already exists"
+        return make_response(jsonify({"status": Err}), 200)
+    pattern = r"^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$"
+    match = _is_match(_email, pattern)
+    if match == None:
+        Err = "E-mail format error"
+        return make_response(jsonify({"status": Err}), 200)
+
+    #phone check
+    pattern = r"^09\d{8}$"
+    match = _is_match(_phone, pattern)
+    if match == None:
+        Err = "phone format error"
+        return make_response(jsonify({"status": Err}), 200)
+
+    #make json
+    new_user = {
+        "account_avator": None,
+        "first_name": _First_name,
+        "last_name":  _Last_name,
+        "nick_name": _Nick_name,
+        "account": _account,
+        "password": _password,
+        "e-mail": _email,
+        "phone": _phone,
+        "birth_date": None,
+        "sex": None,
+        "market_discription": None,
+        "browsing_history": None,
+        "cart": None,
+        "notifications": None,
+        "star": None,
+        "knockroom": None
+    }
+
+    req = MODEL.new(new_user)
+    return make_response(jsonify({"status": "ok"}), 200)
+
+@app.route(f"{MODULE_PREFIX}/login",methods=["POST"])
+def _login():
+    _account = request.get_json().get("account",None)
+    _password = request.get_json().get("password",None)
+    account_MODEL = account.Account()
+    token_MODEL = token.Token()
+    account_req = account_MODEL.get({"account":_account})
+    if account_req != None and _account == account_req['account'] and _password == account_req['password']:
+        token_req = token_MODEL.get({"account":_account})
+        secret = config.getConfig().get("app",{}).get("secret")
+        account_token = bytes.decode(jwt.encode({"account":_account,"timestamp":str(time())},secret))
+        if token_req == None:
+            token_MODEL.new({"account":_account,"token":account_token})
+        else:    
+            token_MODEL.update({"account":_account},{"$set": {"token": account_token}})
+        account_req.update({"password": None, "_id": None})
+        return make_response(jsonify({"account":account_req, "token":account_token}),200)
+    return make_response(jsonify("login failure"), 401)
+
+@app.route(f"{MODULE_PREFIX}/logout",methods=["POST"])
+@account.Account.validate
+def _logout(*args, **kwargs):
+    token_MODEL = token.Token()
+    token_MODEL.update({"account":kwargs["account"]},{"$set":{"token":None}})
+    return make_response(jsonify({"status":"ok"}),200)
+
+@app.route(f"{MODULE_PREFIX}/validate",methods=["POST"])
+def _validate(*args, **kwargs):
+    front_token = request.get_json().get('token',None)
+    db_token = token.Token().get({"token":front_token})
+    secret = config.getConfig().get("app",{}).get("secret")
+    _LOGGER.info("[info] validating token... (from validate API)")
+    if db_token != None and front_token != None and front_token != '' and front_token == db_token.get('token'):
+        _LOGGER.info("Front token: "+front_token)
+        _LOGGER.info("DB token: "+db_token.get('token'))
+        try:
+            token_data = jwt.decode(str.encode(front_token),secret)
+            return make_response({"result": True}, 200)
+        except:
+            return make_response({"result": False}, 200)
+    return make_response({"result": False}, 200)
+
+def send_async_email(app, msg):
+    with app.app_context():
+        get_mail().send(msg)
+
+def _tokenGenerator(N):
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+
+def _is_match(s, pat):
+    return re.findall(pat, s)
+
+def _len_check(s):
+    if len(s) > 5 and len(s) < 21:
+        return True
+    return False
+
+def check_email(_email):
+    pattern = r"^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z]+$"
+    match = re.findall(pattern, _email)
+    return match
+
+@app.route(f'{MODULE_PREFIX}/checkemail', methods=["POST"])
+def _check_email():
+    _email = request.get_json().get("e-mail",None)
+    if not check_email(_email):
+        return make_response(jsonify("E-mail format error"),200)
+    account_req = account_MODEL.get({"e-mail":_email})
+    if account_req is not None and account_req["e-mail"] == _email:
+        _verification_code = _tokenGenerator(6)
+        msg = Message(
+            '[TetrapodShop] Please verify your device!',
+            sender=getConfig().get("app",{}).get("mail_account"),
+            recipients=[_email],
+            body=
+            "Hey "+ account_req["account"]+"!\nTo complete the password reset, please enter the verification code on the device.\n\n\tVerification code: "+_verification_code+"\n\nThanks,\nThe TetrapodShop Team"
+        )
+        thr = Thread(target=send_async_email, args=[app, msg])
+        thr.start()
+        verification_req = verification_MODEL.get({"account":account_req["account"]})
+        if verification_req == None:
+            verification_MODEL.new({"account":account_req["account"],"verification_code":_verification_code})
+        else:
+            verification_MODEL.update({"account":account_req["account"]},{"$set":{"verification_code":_verification_code}})
+        return make_response(jsonify("mail sended"),200)
+    return make_response(jsonify("E-mail not exist"),401)
+
+@app.route(f'{MODULE_PREFIX}/check_verification_code', methods=["POST"])
+def _check_verification_code():
+    _email = request.get_json().get("e-mail",None)
+    if not check_email(_email):
+        return make_response(jsonify("E-mail format error"),200)
+    account_req = account_MODEL.get({"e-mail":_email})
+    if account_req == None:
+        return make_response(jsonify("E-mail not exist"),401)
+    _user_verification = request.get_json().get("verification code",None)
+    db_req = verification_MODEL.get({"account":account_req["account"]})
+    if db_req == None:
+        return make_response(jsonify("e-mail non exist"),401)
+    _db_verification = db_req["verification_code"]
+    if _db_verification == _user_verification:
+        return make_response(jsonify("verification code match"),200)
+    return make_response(jsonify("verification code not match"),401)
+
+@app.route(f'{MODULE_PREFIX}/resetpassword', methods=["POST"])
+def _change_password():
+    _email = request.get_json().get("e-mail",None)
+    if not check_email(_email):
+        return make_response(jsonify("E-mail format error"),200)
+    account_req = account_MODEL.get({"e-mail":_email})
+    if account_req == None:
+        return make_response(jsonify("E-mail not exist"),401)
+    _user_verification = request.get_json().get("verification code",None)
+    db_req = verification_MODEL.get({"account":account_req["account"]})
+    if db_req == None:
+        return make_response(jsonify("e-mail non exist"),401)
+    _db_verification = db_req["verification_code"]
+    if _db_verification != _user_verification:
+        return make_response(jsonify("verification code not match"),401)
+    _password = request.get_json().get("password",None)
+    _confirm_password = request.get_json().get("confirm password",None)
+    pattern = "[a-zA-Z0-9]*"
+    DP = _password ==  _confirm_password
+    LC = _len_check(_password)
+    match = _is_match(_password, pattern)
+    if not (DP and LC and match):
+        Err = "password format error"
+        return make_response(jsonify({"status": Err}), 200)
+    account_MODEL.update({"account":account_req["account"]},{"$set":{"password":_password}})
+    verification_MODEL.delete({"account":account_req["account"]})
+    return make_response(jsonify({"status":"ok"}),200)
---
+++ tetrapod_backend/app/merchant.py
@@ -0,0 +1,86 @@
+from ..db.models import merchant, account
+from ..lib import config
+from .app import *
+from bson.objectid import ObjectId
+import jwt, time, json, re, datetime
+
+MODULE_PREFIX = '/merchant'
+
+@app.route(f"{MODULE_PREFIX}/upload_merchant",methods=["POST"])
+@account.Account.validate
+def _upload_merchant(*args,**kwargs):
+
+    _price = request.get_json().get('price',None)
+    _photo = request.get_json().get('photo',None)
+    _merchant_name = request.get_json().get('merchant_name',None)
+    _count = request.get_json().get('count',None)
+    _discription = request.get_json().get('discription',None)
+    _is_bidding = request.get_json().get('is_bidding',None)
+    _bidding_price = request.get_json().get('bidding_price',None)
+    _bidding_price_perbid = request.get_json().get('bidding_price_perbid',None)
+    _bidding_endtime = request.get_json().get('bidding_endtime',None)
+    _account=kwargs['account']
+
+    match = re.match(r"[1-9][0-9]*", _price)
+    if not(match):
+        return make_response(jsonify({"status": '價格異常'}), 422)
+
+    match = re.match(r"[1-9][0-9]*", _count)
+    if not(match):
+        return make_response(jsonify({"status": '數量異常'}), 422)
+
+    match = re.match(r"[0-1]", _is_bidding)
+    if not(match):
+        return make_response(jsonify({"status": '競標狀態異常'}), 422)
+
+    elif(_is_bidding=='1'):
+        match = re.match(r"[1-9][0-9]*", _bidding_price)
+        if not(match):
+            return make_response(jsonify({"status": '競標價格異常'}), 422)
+
+        match = re.match(r"[1-9][0-9]*", _bidding_price_perbid)
+        if not(match):
+            return make_response(jsonify({"status": '每標價格異常'}), 422)
+
+        try:
+            match_bidding_endtime = datetime.datetime.strptime(_bidding_endtime,"%Y-%m-%d%H:%M")
+            _now = datetime.datetime.now()
+            if(match_bidding_endtime < _now):
+                return make_response(jsonify({"status": '競標時間異常'}), 422)
+        except:
+            return make_response(jsonify({"status": '競標時間異常'}), 422)
+    else:
+        _bidding_price = None
+        _bidding_price_perbid = None
+        _bidding_endtime = None
+    #make json
+    form = {
+        "price":_price,
+        "photo":_photo,
+        "merchant_name":_merchant_name,
+        "count":_count,
+        "discription":_discription,
+        "is_bidding":_is_bidding,
+        "bidding_price":_bidding_price,
+        "bidding_price_perbid":_bidding_price_perbid,
+        "bidding_endtime":_bidding_endtime,
+        "account":_account
+    }
+    MODEL = merchant.Merchant()
+    MODEL.new(form)
+    return make_response(jsonify("upload merchant success"),200)
+
+@app.route(f"{MODULE_PREFIX}/delete_merchant", methods=["POST"])
+@account.Account.validate
+
+def _delete_merchant():
+    data = request.get_json()
+    _merchant_id = data.get("merchant id", "")
+    Err = ""
+    if _merchant_id == "":
+        Err = "no merchant_id"
+        return make_response(jsonify({"status": Err}),200)
+    merchant_model = merchant.Merchant()
+    f = {"_id": ObjectId(_merchant_id)}
+    merchant_model.delete(f)
+    return make_response(jsonify({"status": "ok"}),200)
